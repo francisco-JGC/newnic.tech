565e9c1e3d1ca378b4a06f1d271888327eb71542
Francisco Garcia <jjjchico1@gmail.com>
2024-05-02 01:59:37 -0600

    Update Home component in page.js to include TitleSection component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+import { TitleSection } from '@/components/TitleSection'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container" id="home">
+      <section className="home-content">
+        <TitleSection
+          title="Transformando Ideas en Soluciones Innovadoras y Tecnológicas"
+          description="NewNic es una empresa de desarrollo de software que ofrece soluciones tecnológicas a medida para empresas, destacando por nuestra calidad e innovación para impulsar el éxito empresarial en el mundo digital."
+          small="ven y trabaja con nosotros"
+        />
+      </section>
     </div>
   )
 }
