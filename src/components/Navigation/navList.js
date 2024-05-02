c8cc20fde21e81d491b66aa94de28018179f8cc9
Francisco Garcia <jjjchico1@gmail.com>
2024-05-02 01:59:58 -0600

    Add Navigation component to the project

---
+++ src/components/Navigation/navList.js
@@ -0,0 +1,18 @@
+export const NAVLINK_MENU_LIST = [
+  {
+    name: 'Inicio',
+    href: '#home',
+  },
+  {
+    name: 'Servicios',
+    href: '#services',
+  },
+  {
+    name: 'Beneficios',
+    href: '#benefits',
+  },
+  {
+    name: 'Nosotros',
+    href: '#about',
+  },
+]
