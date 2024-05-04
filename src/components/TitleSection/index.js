cc3f430afa28048ae0fffbb7949f623c8b1b0178
Francisco Garcia <jjjchico1@gmail.com>
2024-05-04 17:14:54 -0600

    style: Update TitleSection component styles
    - Increase the width of the title section to 70%
    - Adjust the text stroke thickness to 1.5px for better visibility

--- src/components/TitleSection/index.js
+++ src/components/TitleSection/index.js
@@ -1,10 +1,31 @@
 import './index.scss'
 
-export const TitleSection = ({ title, description, small }) => {
+export const TitleSection = ({
+  title,
+  description,
+  small,
+  titleSize = 'h3',
+}) => {
   return (
     <section className="title-section">
       <small className="title-section-small">{small}</small>
-      <h1>{title}</h1>
+      {titleSize === 'h1' ? (
+        <h1
+          style={{
+            fontSize: '2em',
+          }}
+        >
+          {title}
+        </h1>
+      ) : (
+        <h2
+          style={{
+            fontSize: '1.4em',
+          }}
+        >
+          {title}
+        </h2>
+      )}
       <p className="title-description">{description}</p>
     </section>
   )
