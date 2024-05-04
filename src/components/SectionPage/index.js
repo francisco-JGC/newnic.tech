0bec0dec0d8facda20fa65e8f1fd3ecc4b93d5b8
Francisco Garcia <jjjchico1@gmail.com>
2024-05-04 16:57:43 -0600

    feat: Add lazy loading for background image in SectionPage component

--- src/components/SectionPage/index.js
+++ src/components/SectionPage/index.js
@@ -1,6 +1,8 @@
 import './index.scss'
 
 import { TitleSection } from '../TitleSection'
+import Image from 'next/image'
+import backgroundBlock from '@/assets/images/background-section.webp'
 export const SectionPage = ({
   title,
   description,
@@ -11,6 +13,9 @@ export const SectionPage = ({
   return (
     <div className={`section-page`}>
       <section className={`section-page-content ${className}`}>
+        <div className="section-page-background">
+          <Image src={backgroundBlock} alt={title} />
+        </div>
         <TitleSection title={title} description={description} small={small} />
         {children}
       </section>
