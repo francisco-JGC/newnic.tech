81c17290f5fbe6d4e9cf79929799afc526a68d67
Francisco Garcia <jjjchico1@gmail.com>
2024-05-04 16:18:29 -0600

    chore: Update home page with SectionPage component and CButton

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,24 @@
-import { TitleSection } from '@/components/TitleSection'
-import Image from 'next/image'
+import './page.scss'
+import { CButton } from '@/components/CButton'
+import { NavLink } from '@/components/Navigation/navLink'
+import { SectionPage } from '@/components/SectionPage'
+import { ProcessItem } from './components/ProcessItem'
 
 export default function Home() {
   return (
     <div className="home-container" id="home">
-      <section className="home-content">
-        <TitleSection
-          title="Transformando Ideas en Soluciones Innovadoras y Tecnológicas"
-          description="NewNic es una empresa de desarrollo de software que ofrece soluciones tecnológicas a medida para empresas, destacando por nuestra calidad e innovación para impulsar el éxito empresarial en el mundo digital."
-          small="ven y trabaja con nosotros"
-        />
-      </section>
+      <SectionPage
+        title="Transformando Ideas en Soluciones Innovadoras y Tecnológicas"
+        description="NewNic es una empresa de desarrollo de software que ofrece soluciones tecnológicas a medida para empresas, destacando por nuestra calidad e innovación para impulsar el éxito empresarial en el mundo digital."
+        small="ven y trabaja con nosotros"
+        className={'home-section'}
+      >
+        <CButton>
+          <NavLink href="#contact">Contáctanos</NavLink>
+        </CButton>
+
+        <ProcessItem />
+      </SectionPage>
     </div>
   )
 }
