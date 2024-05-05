0dae3abf39cb503ae937e1273d40355acd730b21
Francisco Garcia <jjjchico1@gmail.com>
2024-05-05 12:52:13 -0600

    feat: Add mobile menu functionality to Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,38 @@
+'use client'
 import './index.scss'
 import { Navigation } from '../Navigation'
 import newnicLogo from '@/assets/logo/newnicweb.webp'
 import Image from 'next/image'
+import { useState } from 'react'
+import menuLinesIcon from '@/assets/icons/menu-lines.svg'
 
 export const Header = () => {
+  const [openMenu, setOpenMenu] = useState(true)
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu)
+  }
+
   return (
     <header className="header">
       <div className="header-container">
-        <Image src={newnicLogo} alt="Newnic Logo" />
+        <div className="logo-container">
+          <Image src={newnicLogo} alt="Newnic Logo" />
+        </div>
 
-        <section className="header-navigation">
+        <section className={`header-navigation ${openMenu ? 'open' : ''}`}>
           <Navigation />
         </section>
+
+        <div className="menu-mobile" onClick={handleOpenMenu}>
+          <Image src={menuLinesIcon} alt="Menu Icon" />
+
+          {openMenu && (
+            <div className="menu-mobile-overlay" onClick={handleOpenMenu}>
+              <Navigation />
+            </div>
+          )}
+        </div>
       </div>
     </header>
   )
