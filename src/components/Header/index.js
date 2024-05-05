121028e3a87b9f5d8321f4c65d9c215836ba7460
Francisco Garcia <jjjchico1@gmail.com>
2024-05-05 12:57:57 -0600

    feat: Improve lazy loading for images in Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,18 +3,34 @@ import './index.scss'
 import { Navigation } from '../Navigation'
 import newnicLogo from '@/assets/logo/newnicweb.webp'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import menuLinesIcon from '@/assets/icons/menu-lines.svg'
 
 export const Header = () => {
   const [openMenu, setOpenMenu] = useState(true)
+  const headerRef = useRef(null)
 
   const handleOpenMenu = () => {
     setOpenMenu(!openMenu)
   }
 
+  let lastScrollTop = 0
+
+  window.addEventListener('scroll', () => {
+    const header = headerRef.current
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+    if (scrollTop > lastScrollTop) {
+      header?.classList.add('header-container--hide')
+    } else {
+      header?.classList.remove('header-container--hide')
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+  })
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <div className="header-container">
         <div className="logo-container">
           <Image src={newnicLogo} alt="Newnic Logo" />
