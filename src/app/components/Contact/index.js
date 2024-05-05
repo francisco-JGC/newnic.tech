c8112b3e32f6451ca1f960e79766c31b509d5d72
Francisco Garcia <jjjchico1@gmail.com>
2024-05-05 00:25:41 -0600

    feat: Add Contact component and social media icons
    The code changes add the Contact component to display contact information on the Home page. Additionally, new SVG icons for Facebook, Instagram, LinkedIn, and Location are added to enhance the visual appearance and functionality of the website.

---
+++ src/app/components/Contact/index.js
@@ -0,0 +1,154 @@
+'use client'
+import './index.scss'
+import { CButton } from '@/components/CButton'
+import { ContentIcon } from '@/components/ContentIcon'
+import { NavLink } from '@/components/Navigation/navLink'
+import { useState } from 'react'
+import facebook from '@/assets/icons/facebook.svg'
+import instagram from '@/assets/icons/instagram.svg'
+import linkedin from '@/assets/icons/linkedin.svg'
+import clockIcon from '@/assets/icons/clock-primary.svg'
+import location from '@/assets/icons/location.svg'
+import Image from 'next/image'
+
+export const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [service, setService] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({ name, email, service, message })
+  }
+
+  return (
+    <div className="contact-section">
+      <div className="social-media">
+        <div className="social-media-icons">
+          <NavLink
+            href="https://www.facebook.com/newnicsoftware"
+            target="_blank"
+          >
+            <ContentIcon icon={facebook} />
+          </NavLink>
+
+          <NavLink
+            href="https://www.instagram.com/newnicsoftware/"
+            target="_blank"
+          >
+            <ContentIcon icon={instagram} />
+          </NavLink>
+
+          <NavLink
+            href="https://www.linkedin.com/company/newnicsoftware/"
+            target="_blank"
+          >
+            <ContentIcon icon={linkedin} />
+          </NavLink>
+        </div>
+
+        <div className="social-media-locations">
+          <div className="contact-info">
+            <Image
+              src={clockIcon}
+              alt="clock"
+              loading="lazy"
+              width={25}
+              height={25}
+            />
+
+            <div>
+              <h3>Horario de atención</h3>
+              <span>Lunes - Viernes: 8:00 am - 5:00 pm</span>
+            </div>
+          </div>
+          <div className="contact-info">
+            <Image
+              src={location}
+              alt="location"
+              loading="lazy"
+              width={25}
+              height={25}
+            />
+
+            <div>
+              <h3>Ubicación</h3>
+              <span>León - Nicaragua </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">
+            Nombre <span>*</span>
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder='Ejemplo: "Juan Pérez"'
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">
+            Email <span>*</span>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Ejemplo: "email@gmail.com"'
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="service">
+            Servicio <span>*</span>
+          </label>
+          <select
+            id="service"
+            name="service"
+            value={service}
+            onChange={(e) => setService(e.target.value)}
+            required
+          >
+            <option value="" selected disabled>
+              Selecciona un servicio
+            </option>
+            <option value="web">Desarrollo Web</option>
+            <option value="mobile">Desarrollo Móvil</option>
+            <option value="desktop">Desarrollo de Escritorio</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">
+            Mensaje <span>*</span>
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Escribe tu mensaje aquí"
+            required
+          ></textarea>
+        </div>
+
+        <div>
+          <CButton>Enviar</CButton>
+        </div>
+      </form>
+    </div>
+  )
+}
