735e0117fb9ef3e2002748c4e1a84feac66cb9f5
Francisco Garcia <jjjchico1@gmail.com>
2024-05-04 22:31:49 -0600

    style: Update TitleSection component to support different title positions
    - Add positionTitle prop to TitleSection component to specify the position of the title
    - Implement center, left, and right positions with corresponding styles
    - Update section style to apply the specified position

--- src/components/TitleSection/index.js
+++ src/components/TitleSection/index.js
@@ -5,9 +5,30 @@ export const TitleSection = ({
   description,
   small,
   titleSize = 'h3',
+  positionTitle = 'center',
 }) => {
+  const POSITIONS_TITLE = {
+    center: {
+      textAlign: 'center',
+      alignItems: 'center',
+    },
+    left: {
+      textAlign: 'left',
+      alignItems: 'flex-start',
+    },
+    right: {
+      textAlign: 'right',
+      alignItems: 'flex-end',
+    },
+  }
+
   return (
-    <section className="title-section">
+    <section
+      className="title-section"
+      style={{
+        ...POSITIONS_TITLE[positionTitle],
+      }}
+    >
       <small className="title-section-small">{small}</small>
       {titleSize === 'h1' ? (
         <h1
