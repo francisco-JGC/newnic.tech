e3312d9a518b23557eb0d239cb28f6f219c9d34e
Francisco Garcia <jjjchico1@gmail.com>
2024-05-04 23:42:19 -0600

    style: Update ContentIcon component to use smaller icon size
    The code changes in the ContentIcon component update the size of the icon to 25x25 pixels, improving the visual appearance and reducing the overall space occupied by the icon.

--- src/components/ContentIcon/index.js
+++ src/components/ContentIcon/index.js
@@ -3,8 +3,18 @@ import Image from 'next/image'
 
 export const ContentIcon = ({ icon }) => {
   return (
-    <DivContent className="content-icon">
-      <Image src={icon} alt="icon" loading="lazy" width={100} height={100} />
+    <DivContent
+      className="content-icon"
+      style={{
+        width: '50px',
+        height: '50px',
+        padding: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Image src={icon} alt="icon" loading="lazy" width={25} height={25} />
     </DivContent>
   )
 }
